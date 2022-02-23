dbfad8354c8c99d16523c73f6d589c05bb2fd25b
CesarAlexisOsunaHernandez <79597696+CesarAlexisOsunaHernandez@users.noreply.github.com>
2022-02-22 17:50:10 -0800

    ver 0.2.2b

--- Practica2/Assets/Scrips/Problema.cs
+++ Practica2/Assets/Scrips/Problema.cs
@@ -4,26 +4,32 @@ using UnityEngine;
 
 public class Problema : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int[] input;
+	public int num;
+	
+	// Start is called before the first frame update
     void Start()
     {
-        int[] nums = {1,2,3,4,5,6};
-		int z = 7;
 		
-        for(int i = 0; i < 6; i++){
-			if(nums[i] < z){
-				for(int j = i; j < 6; j++){
-					if(nums[i] + nums[j] == z){
-						Debug.Log(nums[i] + " " + nums[j]);
-					}
-				}
-			}
-        } 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.X)){
+			RevisarSuma(input, num);
+		}
     }
+	
+	private void RevisarSuma(int[] arr, int num){
+		for(int i = 0; i < arr.Length; i++){
+			if(arr[i] < num){
+				for(int j = i; j < arr.Length; j++){
+					if(arr[i] + arr[j] == num){
+						Debug.Log(arr[i] + " " + arr[j]);
+					}
+				}
+			}
+        } 
+	}
 }
